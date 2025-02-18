e5da78cee5688c67ca4009e4b9c68e179bd65b5b
dani <oteroriveradaniel@gmail.com>
2025-02-18 20:31:39 +0100

    Button click event

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className='flex-sandbox'>
       <Button 
+        onClick={() => console.log("Clicked!")}
         text='Something' />
     </div>
   );
--- src/components/base/Button.tsx
+++ src/components/base/Button.tsx
@@ -1,12 +1,16 @@
+import { MouseEventHandler } from 'react';
 import './base.css';
 
 type ButtonParams = {
   text: string;
+  onClick: MouseEventHandler;
 }
 
 function Button({...params}: ButtonParams) {
   return (
-    <button className="dor-button">
+    <button 
+      className="dor-button" 
+      onClick={params.onClick}>
       { params.text }
     </button>
   );
